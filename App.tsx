ece6e98fdf057473c8fc9eac333c01718838311c
Victor Gnoato <85642178+gnoatx@users.noreply.github.com>
2024-10-09 19:45:01 -0300

    Não consigo fazer o shadow aparecer

--- App.tsx
+++ App.tsx
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.neueBox}>
+        <Text style={{color: '#eee', fontSize: 18}}>Qual É O Tema?</Text>
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +15,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#111',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  neueBox: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '50%',
+    height: '15%',
+    borderRadius: 30,
+    shadowColor: '#fff',
+    shadowOffset: {width: 500, height: 500},
+    shadowRadius: 55,
+    backgroundColor: '#555',
   },
 });
